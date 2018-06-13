7ad028c58f048af321db171826d655e575a910d9
Adarsh Pai <adarsh9pai@gmail.com>
2018-06-13 13:49:18 -0500

    BinarySearchLast function added and IncrementByIndex done.

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int BinarySearchLast(int arr[], int low, int high, int n, int len){
+
+    if(high>=low){
+        int mid = low + (high-low)/2; //to avoid integer overflow
+        if((mid == len-1 || n<arr[mid+1]) && arr[mid]==n){
+            return mid;
+        }
+        else if(n <arr[mid]){
+            return BinarySearchLast(arr,low,(mid-1),n,len);
+        }
+        else{
+            return BinarySearchLast(arr,(mid+1),high,n,len);
+        }
+    }
+    else{
+        return -1;
+    }
+}
+
 
 void AscendingIndexValue(int *map, int *index, int *count, int n){
     int i = 0;
@@ -14,6 +33,35 @@ void AscendingCounterValue(int *index, int *count, int n){
     for(i=0;i<n;i++){
         printf("%d %d\n", index[i], count[i]);
     }
+}
+
+void IncrementByIndex(int *map,int *index, int *count, int n,int length){
+
+      int first = 0, last = length-1;
+      int x = BinarySearchLast(count,first,last,count[n],length);
+      int temp;
+
+      temp = index[n];
+      index[n] = index[x];
+      index[x] = temp;
+
+      temp = map[n];
+      map[n] = map[x];
+      map[n] = temp;
+      
+      count[x] += 1;
+
+}
+
+void DecrementByIndex(int *map,int *index, int *count, int n,int length){
+      
+
+}
+
+int NumberOfCounters(int *counter, int i, int j, int n){
+
+   int count=0;
+   return count;
 }
 
 int main(){
@@ -48,14 +96,18 @@ while(j==0){
     else{
         switch(operation){
         case 1:
+            printf("print by index\n");
             AscendingIndexValue(map,index,count,n);
+            printf("-------\n");
             break;
         case 2:
+            printf("print by count\n");
             AscendingCounterValue(index,count,n);
+            printf("-------\n");
             break;
         case 3:
             arg1 = strtol(ptr, &ptr, 10);
-         //   printf(" %d",arg1);
+            IncrementByIndex(map,index,count,arg1,n);
             break;
         case 4:
             arg1 = strtol(ptr, &ptr, 10);

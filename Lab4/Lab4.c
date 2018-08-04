0ef1ecd652f93d99583db6f1eedc4bc492ca79b3
Adarsh Pai <adarsh9pai@gmail.com>
2018-08-04 05:40:31 -0500

    Initial Binary Tree Implemented.

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
-#define SIZE 200
+#include <stdlib.h>
+
+struct Node{
+	char Color;
+	struct Node* LeftChild;
+	struct Node* RightChild;
+};
+
+struct Node* BuildTree(){
+	char Color;
+	scanf("%c",&Color);
+	struct Node* Child;
+	if(Color == '.'){
+		return NULL;
+	}
+	Child = (struct Node*)malloc(sizeof(struct Node));
+	Child->Color = Color;
+	Child->LeftChild = BuildTree();
+	Child->RightChild = BuildTree();
+
+	return Child;
+}
+
+void PreOrderTraversal(struct Node* Root){
+	if(Root != NULL){
+		printf("%c",Root->Color);
+		PreOrderTraversal(Root->LeftChild);
+		PreOrderTraversal(Root->RightChild);
+	}
+	else if(Root == NULL){
+		printf(".");
+	}
+}
 
 int main(){
+	struct Node* TreeRoot;
+	TreeRoot = BuildTree();
+	PreOrderTraversal(TreeRoot);
 
-	char *line = malloc(sizeof(char) * SIZE);
-	fgets(line,100,stdin);
-
-	free(line);
 }
